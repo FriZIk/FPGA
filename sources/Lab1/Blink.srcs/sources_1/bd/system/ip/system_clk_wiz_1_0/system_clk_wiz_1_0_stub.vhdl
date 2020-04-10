284278e0943320a2aff194af16453b553d069af4
frizik <tester251100@gmail.com>
2020-04-10 13:38:25 +0300

    10.04.2020

---
+++ sources/Lab1/Blink.srcs/sources_1/bd/system/ip/system_clk_wiz_1_0/system_clk_wiz_1_0_stub.vhdl
@@ -0,0 +1,30 @@
+-- Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2016.3 (lin64) Build 1682563 Mon Oct 10 19:07:26 MDT 2016
+-- Date        : Wed Feb 19 21:18:09 2020
+-- Host        : frizik running 64-bit Arch Linux
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/frizik/MOPEVM/fpga/Blink/Blink.srcs/sources_1/bd/system/ip/system_clk_wiz_1_0/system_clk_wiz_1_0_stub.vhdl
+-- Design      : system_clk_wiz_1_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35ticsg324-1L
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity system_clk_wiz_1_0 is
+  Port ( 
+    clk_out1 : out STD_LOGIC;
+    locked : out STD_LOGIC;
+    clk_in1 : in STD_LOGIC
+  );
+
+end system_clk_wiz_1_0;
+
+architecture stub of system_clk_wiz_1_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_out1,locked,clk_in1";
+begin
+end;
