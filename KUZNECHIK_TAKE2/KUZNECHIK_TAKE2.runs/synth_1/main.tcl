12e43725e707a8432b306a931100afc3502933f0
BuildTools <unconfigured@null.spigotmc.org>
2020-09-29 11:42:26 +0300

    succesfuly made table conversion and xor on output

--- KUZNECHIK_TAKE2/KUZNECHIK_TAKE2.runs/synth_1/main.tcl
+++ KUZNECHIK_TAKE2/KUZNECHIK_TAKE2.runs/synth_1/main.tcl
@@ -70,7 +70,6 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 1
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
