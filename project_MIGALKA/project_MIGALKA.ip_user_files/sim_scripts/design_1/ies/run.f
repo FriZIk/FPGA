9c62d652804df46531a2b037ba947bdd9c6c454d
eqrewqr <dewqdewq>
2020-10-13 12:33:14 +0300

    Delete useless files

--- project_MIGALKA/project_MIGALKA.ip_user_files/sim_scripts/design_1/ies/run.f
+++
@@ -1,4 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../bd/design_1/sim/design_1.vhd" \
--endlib
-
--- project_MIGALKA/project_MIGALKA.srcs/sources_1/bd/design_1/synth/design_1.vhd
+++
@@ -1,9 +0,0 @@
---Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
-----------------------------------------------------------------------------------
---Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
---Date        : Thu Sep 10 10:29:14 2020
---Host        : DESKTOP-IO9CP06 running 64-bit major release  (build 9200)
---Command     : generate_target design_1.bd
---Design      : design_1
---Purpose     : IP block netlist
-----------------------------------------------------------------------------------
