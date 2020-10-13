1840e32d812f6530bc22e8728f50c38c30af4257
eqrewqr <dewqdewq>
2020-10-13 15:08:59 +0300

    Error with ports from axi

---
+++ Final_Microblaze_Plus_IP-CORE/Final_Microblaze_Plus_IP-CORE.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,91 @@
+--Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2016.2 (win64) Build 1577090 Thu Jun  2 16:32:40 MDT 2016
+--Date        : Tue Oct 13 14:40:09 2020
+--Host        : cheh2-111-08 running 64-bit Service Pack 1  (build 7601)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+  port (
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 15 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    outputBlockPort : out STD_LOGIC_VECTOR ( 0 to 15 );
+    outputBlockPort_1 : out STD_LOGIC_VECTOR ( 0 to 15 );
+    reset : in STD_LOGIC;
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  port (
+    ddr3_sdram_dq : inout STD_LOGIC_VECTOR ( 15 downto 0 );
+    ddr3_sdram_dqs_p : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_dqs_n : inout STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_addr : out STD_LOGIC_VECTOR ( 13 downto 0 );
+    ddr3_sdram_ba : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    ddr3_sdram_ras_n : out STD_LOGIC;
+    ddr3_sdram_cas_n : out STD_LOGIC;
+    ddr3_sdram_we_n : out STD_LOGIC;
+    ddr3_sdram_reset_n : out STD_LOGIC;
+    ddr3_sdram_ck_p : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_ck_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cke : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_cs_n : out STD_LOGIC_VECTOR ( 0 to 0 );
+    ddr3_sdram_dm : out STD_LOGIC_VECTOR ( 1 downto 0 );
+    ddr3_sdram_odt : out STD_LOGIC_VECTOR ( 0 to 0 );
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC;
+    outputBlockPort : out STD_LOGIC_VECTOR ( 0 to 15 );
+    outputBlockPort_1 : out STD_LOGIC_VECTOR ( 0 to 15 )
+  );
+  end component design_1;
+begin
+design_1_i: component design_1
+     port map (
+      ddr3_sdram_addr(13 downto 0) => ddr3_sdram_addr(13 downto 0),
+      ddr3_sdram_ba(2 downto 0) => ddr3_sdram_ba(2 downto 0),
+      ddr3_sdram_cas_n => ddr3_sdram_cas_n,
+      ddr3_sdram_ck_n(0) => ddr3_sdram_ck_n(0),
+      ddr3_sdram_ck_p(0) => ddr3_sdram_ck_p(0),
+      ddr3_sdram_cke(0) => ddr3_sdram_cke(0),
+      ddr3_sdram_cs_n(0) => ddr3_sdram_cs_n(0),
+      ddr3_sdram_dm(1 downto 0) => ddr3_sdram_dm(1 downto 0),
+      ddr3_sdram_dq(15 downto 0) => ddr3_sdram_dq(15 downto 0),
+      ddr3_sdram_dqs_n(1 downto 0) => ddr3_sdram_dqs_n(1 downto 0),
+      ddr3_sdram_dqs_p(1 downto 0) => ddr3_sdram_dqs_p(1 downto 0),
+      ddr3_sdram_odt(0) => ddr3_sdram_odt(0),
+      ddr3_sdram_ras_n => ddr3_sdram_ras_n,
+      ddr3_sdram_reset_n => ddr3_sdram_reset_n,
+      ddr3_sdram_we_n => ddr3_sdram_we_n,
+      outputBlockPort(0 to 15) => outputBlockPort(0 to 15),
+      outputBlockPort_1(0 to 15) => outputBlockPort_1(0 to 15),
+      reset => reset,
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
+    );
+end STRUCTURE;
